888784a431428a2816f95e7296cd39fe1ce97ae6
Sumshi <sumayasomow@gmail.com>
2023-06-17 17:55:28 +0000

    simple python programs

---
+++ Day_001/string.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+name = input("What is your name? ")
+print(len(name))
---
+++ Day_001/swap.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+a = input("a: ")
+b = input("b: ")
+a, b = b, a
+print("a: " + a)
+print("b: " + b)
+
+#or just introduce another variable eg
+# c = a
+# a = b
+# b = c
