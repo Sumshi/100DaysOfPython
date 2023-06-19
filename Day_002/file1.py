15fd3fca60e3d9fd1c2ac3d78554eab8d2a4b159
Sumshi <sumayasomow@gmail.com>
2023-06-19 11:04:24 +0000

    task 4 done

--- Day_002/file1.py
+++ Day_002/file1.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 street_name = "Abbey Road"
 print(street_name[4] + street_name[7])
+
+name = input("what is ur name")
+print(f"hello, " + name)
+
+age = 12
+print(f"you are {age} years old")
