47e9ee73a507177815171af9ccc0cb3b56a0daaf
Sumshi <sumayasomow@gmail.com>
2023-06-25 12:37:08 +0000

    who pays the bill

---
+++ Day_004/Banker_Roulette.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Import the random module here
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+num_items = len(names)
+#Generate random numbers between 0 and the last index. 
+random_choice = random.randint(0, num_items)
+#Pick out random person from list of names using the random number.
+person_who_will_pay = names[random_choice]
+print(f"{person_who_will_pay} is going to buy the meal today!")
