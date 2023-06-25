9b28754eafa44d98df722fdd9bfa61f7d87ad9fd
Sumshi <sumayasomow@gmail.com>
2023-06-25 18:01:14 +0000

    game over

--- Day_003/love_calc.py
+++ Day_003/love_calc.py
@@ -6,28 +6,25 @@ name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+first_digit = t + r + u + e
 
-combined_string = name1 + name2
-lower_case_string = combined_string.lower()
-t = lower_case_string.count("t")
-r = lower_case_string.count("r")
-u = lower_case_string.count("u")
-e = lower_case_string.count("e")
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
+second_digit = l + o + v + e
 
-true = t + r + u + e
+score = int(str(first_digit) + str(second_digit))
 
-l = lower_case_string.count("l")
-o = lower_case_string.count("o")
-v = lower_case_string.count("v")
-e = lower_case_string.count("e")
-
-love = l + o + v + e
-
-love_score  = int(str(love) + str(true))
-
-if (love_score < 10) or (love_score > 90):
-    print(f"Your score is {love_score}, you go together like coke and mentos.")
-elif (love_score >= 40) and (love_score <= 50):
-    print(f"Your score is {love_score}, you are alright together.")
+if (score < 10) or (score > 90):
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+  print(f"Your score is {score}, you are alright together.")
 else:
-    print(f"Your score is {love_score}")
+  print(f"Your score is {score}.")
