8c1a51116e0a461b766fb86d9969758dae02dde2
Sumshi <sumayasomow@gmail.com>
2023-06-29 11:35:35 +0000

    oop

--- Object_Classes/README.md
+++ Object_Classes/README.md
@@ -1,1 +1,4 @@
 	This repo will contain object oriented concepts from freecodecamp
+I stopped my 100daysofcode with udemy to focus on 00P this week but will continue it
+
+      RESOURCE USED: javapoint python oop
---
+++ Object_Classes/test1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print("Hello, my name is " + self.name)
+
+p1 = Person("maya", 20)
+p1.greet()
