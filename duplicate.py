05fea4441c4a4df99ad6e1dcff957de3b8ec3adb
Sumshi <sumayasomow@gmail.com>
2023-07-19 04:05:18 +0000

    gets the duplicate numbers

--- duplicate.py
+++ duplicate.py
@@ -4,8 +4,8 @@
 
 numbers = [3,2,6,2,4,3,6,8,5,9]
 
-for i in range(len(numbers)):
-    for j in range(i + 1, len(numbers)):
+for i in range(len(numbers)): # iterates over the entire list
+    for j in range(i + 1, len(numbers)): # iterates over each index
         if numbers[i] == numbers[j]:
             print(f"number {numbers[i]} is a duplicate")
             break
